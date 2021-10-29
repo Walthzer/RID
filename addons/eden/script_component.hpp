85259be1a1a780dd65405e881883e82f50fd7128
Walthzer <s.v.feitsma@gmail.com>
2021-10-29 19:49:03 +0200

    initial

---
+++ addons/eden/script_component.hpp
@@ -0,0 +1,18 @@
+#define COMPONENT eden
+#define COMPONENT_BEAUTIFIED eden
+#include "\z\rid\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define CBA_DEBUG_SYNCHRONOUS
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_EDEN
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_EDEN
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_EDEN
+#endif
+
+#include "\z\rid\addons\main\script_macros.hpp"
